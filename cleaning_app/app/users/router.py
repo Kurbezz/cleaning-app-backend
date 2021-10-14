b6f84032d10a08f5eb66b26928029c0d014d7e21
Kurbanov Bulat <kurbanovbul@gmail.com>
2021-10-14 13:39:04 +0300

    Add token refresh view

--- cleaning_app/app/users/router.py
+++ cleaning_app/app/users/router.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Depends, Response
 from fastapi_users.fastapi_users import FastAPIUsers
 from fastapi_users.authentication.jwt import JWTAuthentication
     
@@ -22,12 +22,23 @@ fastapi_users = FastAPIUsers(
     UserDB,
 )
 
+
+router = APIRouter(
+    tags=["auth"]
+)
+
+@router.post("/auth/jwt/refresh")
+async def refresh_jwt(response: Response, user=Depends(fastapi_users.current_user(active=True))):
+    return await jwt_authentication.get_login_response(user, response, get_user_manager())
+
+
 def init_auth_routes(app: FastAPI):
     app.include_router(
         fastapi_users.get_auth_router(jwt_authentication),
         prefix='/auth/jwt',
         tags=["auth"],
     )
+    app.include_router(router)
     app.include_router(
         fastapi_users.get_register_router(),
         prefix="/auth",
