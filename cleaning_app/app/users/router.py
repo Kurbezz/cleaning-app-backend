2324c7bc801b2ae7adf5eb18c6a617b95e5d7a1a
Kurbanov Bulat <kurbanovbul@gmail.com>
2021-10-15 21:30:36 +0300

    Fix docs login

--- cleaning_app/app/users/router.py
+++ cleaning_app/app/users/router.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter, Depends, Response
 from fastapi_users.fastapi_users import FastAPIUsers
 from fastapi_users.authentication.jwt import JWTAuthentication
-    
+
 from core.config import env_config
 
 from app.users.serializers import User, UserRegister, UserUpdate, UserDB
@@ -10,7 +10,9 @@ from app.users.manager import get_user_manager
 
 SECRET = env_config.SECRET
 
-jwt_authentication = JWTAuthentication[UserRegister, UserDB](secret=SECRET, lifetime_seconds=3600)
+jwt_authentication = JWTAuthentication[UserRegister, UserDB](
+    secret=SECRET, lifetime_seconds=3600, tokenUrl="/api/auth/jwt/login"
+)
 
 
 fastapi_users = FastAPIUsers(
@@ -23,34 +25,31 @@ fastapi_users = FastAPIUsers(
 )
 
 
-router = APIRouter(
-    tags=["auth"]
-)
+router = APIRouter(tags=["auth"])
+
 
 @router.post("/api/auth/jwt/refresh")
-async def refresh_jwt(response: Response, user=Depends(fastapi_users.current_user(active=True))):
-    return await jwt_authentication.get_login_response(user, response, get_user_manager())
+async def refresh_jwt(
+    response: Response, user=Depends(fastapi_users.current_user(active=True))
+):
+    return await jwt_authentication.get_login_response(
+        user, response, get_user_manager()
+    )
 
 
 def init_auth_routes(app: FastAPI):
+    prefix = "/api/auth"
+
     app.include_router(
         fastapi_users.get_auth_router(jwt_authentication),
-        prefix='/api/auth/jwt',
+        prefix="/api/auth/jwt",
         tags=["auth"],
     )
     app.include_router(router)
     app.include_router(
-        fastapi_users.get_register_router(),
-        prefix="/api/auth",
-        tags=["auth"]
+        fastapi_users.get_register_router(), prefix=prefix, tags=["auth"]
     )
+    app.include_router(fastapi_users.get_verify_router(), prefix=prefix, tags=["auth"])
     app.include_router(
-        fastapi_users.get_verify_router(),
-        prefix="/api/auth",
-        tags=["auth"]
-    )
-    app.include_router(
-        fastapi_users.get_reset_password_router(),
-        prefix="/api/auth",
-        tags=["auth"]
+        fastapi_users.get_reset_password_router(), prefix=prefix, tags=["auth"]
     )
