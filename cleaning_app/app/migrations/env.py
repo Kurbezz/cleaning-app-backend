632c75524b89b3d749830b943760862026048e0c
Keny <kenyda25@gmail.com>
2021-10-17 00:33:50 +0300

    Fix lint

--- cleaning_app/app/migrations/env.py
+++ cleaning_app/app/migrations/env.py
@@ -54,7 +54,9 @@ def run_migrations_online():
     connectable = create_engine(DATABASE_URL)
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
